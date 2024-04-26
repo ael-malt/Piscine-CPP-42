f8c180f879f2f3f34ded93e7759e6f2d09f16fc1
ael-malt <thenono1111@gmail.com>
2024-04-26 22:25:15 +0200

    added some comments to ex01

--- CPP09/ex01/srcs/RPN.cpp
+++ CPP09/ex01/srcs/RPN.cpp
@@ -5,6 +5,7 @@ RPN::RPN(string input) {
 }
 
 float	RPN::checkNumberSize(string::iterator inputIt) {
+	//Check the size of the current member and return it
 	for (inputIt i = 0; i < count; i++)
 	{
 		/* code */
@@ -23,16 +24,27 @@ void RPN::parseInput(string input) {
 	if (input.find_first_of(OPERATORS) == std::string::npos)
 		throw(RPN::MissingOperators());
 	
-	cout << input << endl;
-	
+	// Check if amount of number and operators correspond
+
+	// Check if their order is correct ex: 
+	// 1 1 + 2 - 3 - 1 is correct.
+	// 1 + 1 + 2 - 3 - 1 is NOT correct.
+
 	string::iterator inputIt = input.begin();
 
 	for (; inputIt != input.end(); inputIt++)
 	{
 		for (; *inputIt != ' '; inputIt++);
-		
+		// Check if current pos is a nb or an op
+
+		// If its a number get it's size
 		checkNumberSize(inputIt);
-		this->_inputStack.push(std::atof(*inputIt.c_str()));
+		// After checking the size get the number from inputIt to nbSize
+
+		//If it's an operator add it to the stack
+
+		// Push number or operator to the stack
+		// this->_inputStack.push(std::atof(*inputIt.c_str()));
 	}
 	cout << _inputStack.top() << endl;
 	
