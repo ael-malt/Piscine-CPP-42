e27029e40108a453e7768cf4497d9c7a1bddab5f
ael-malt <thenono1111@gmail.com>
2024-04-28 17:31:05 +0200

    ginished

--- CPP09/ex01/includes/RPN.hpp
+++ CPP09/ex01/includes/RPN.hpp
@@ -10,11 +10,9 @@
 # include <algorithm>
 # include <limits.h>
 # include <sstream>
+
 // #include 
 
-#define NUMBERS "0123456789"
-#define OPERATORS "+-/"
-#define NUMOP "0123456789+-/"
 
 using std::cout;
 using std::cerr;
@@ -27,13 +25,23 @@ using std::stack;
 class RPN
 {
 private:
+	// stack<float> _tmpStack;
 	stack<float> _inputStack;
+	const string _NUMBERS;
+	const string _OPERATORS;
+	const string _NUMOP;
 public:
 	RPN(string input);
 	~RPN();
 
-	void	parseInput(string input);
-	float	checkNumberSize(string::iterator inputIt);
+	void			parseInput(string input);
+	void			countNumOPs(string input);
+	unsigned int	checkNumberSize(string::iterator inputIt);
+	void			addNumberToStack(string::iterator inputIt, unsigned int NumberSize);
+	void			calculateStack(char op);
+	float operate(float nb1, float nb2, char op);
+
+
 	class MissingArgument : public exception {
 		public: virtual const char* what() const throw() { return (RED "Error: Missing argument\n	Try with: ./RPN \"1 + 1\"" DEFAULT); }
 	};
@@ -44,13 +52,72 @@ public:
 		public: virtual const char* what() const throw() { return (RED "Error: Input is empty" DEFAULT); }
 	};
 	class IncorrectInput : public exception {
-		public: virtual const char* what() const throw() { return (RED "Error: Incorrect input, only these charachters are allowed: " MAGENTA "0123456789+-/" DEFAULT); }
+		public: virtual const char* what() const throw() { return (RED "Error: Incorrect input, only these charachters are allowed: " MAGENTA "0123456789.+-/" DEFAULT); }
 	};
 	class MissingNumbers : public exception {
 		public: virtual const char* what() const throw() { return (RED "Error: Incorrect input, no numbers were found" DEFAULT); }
 	};
 	class MissingOperators : public exception {
 		public: virtual const char* what() const throw() { return (RED "Error: Incorrect input, no operators were found" DEFAULT); }
+	};
+	class UnexpectedChar : public exception
+	{
+		private:
+			string _msg;
+			string _unexpectedStr;
+		public: 
+			virtual ~UnexpectedChar() throw() {}
+			UnexpectedChar(char unexpected)
+			{
+				_unexpectedStr = unexpected;
+				_msg = RED "Error: unexpected Character found in input instead of a space: " MAGENTA "'" + this->_unexpectedStr + "'" DEFAULT;
+			}
+			const char* what() const throw() { return (_msg.c_str()); }
+
+	};
+	class NumberTooBig : public exception
+	{
+		private: string _msg;
+		public: 
+			virtual ~NumberTooBig() throw() {}
+			NumberTooBig(float number) {
+				std::ostringstream convert;
+				convert << number;
+				string nbStr = convert.str();
+				this->_msg = RED "Error: Number too Big: " MAGENTA "'" + nbStr + "'" DEFAULT;
+			}
+			const char* what() const throw() { return (_msg.c_str()); }
+
+	};
+	class DotAtBeginningOfNumber : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: Cannot have a dot '.' at the beginning of a number" DEFAULT); }
+	};
+	class TooManyDots : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: Too many dots '.' in a number" DEFAULT); }
+	};
+	class NumberEndsWithDot : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: A number cannot end with a dot '.'" DEFAULT); }
+	};
+	class NotEnoughNumbers : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: Not enough numbers in input\n	Need at least 2 numbers" DEFAULT); }
+	};
+	class TooManyNumbers : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: Too many numbers in input" DEFAULT); }
+	};
+	class TooManyOperators : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: Too many operators in input" DEFAULT); }
+	};
+	class ConsecutiveNumbers : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: Consecutive numbers found after first two positions" DEFAULT); }
+	};
+	class ConsecutiveOperators : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: Consecutive operators found after first two positions" DEFAULT); }
+	};
+	class OperatorsAtBeginning : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: Found Operator in one of the first two positions" DEFAULT); }
+	};
+	class BadOperator : public exception {
+		public: virtual const char* what() const throw() { return (RED "Error: Found Bad Operator" DEFAULT); }
 	};
 };
 
--- CPP09/ex01/srcs/RPN.cpp
+++ CPP09/ex01/srcs/RPN.cpp
@@ -1,53 +1,141 @@
 #include "RPN.hpp"
 
-RPN::RPN(string input) {
+RPN::RPN(string input) : _NUMBERS("0123456789."), _OPERATORS("+-/*"), _NUMOP("0123456789.+-/*") {
 	parseInput(input);
 }
 
-float	RPN::checkNumberSize(string::iterator inputIt) {
-	//Check the size of the current member and return it
-	for (inputIt i = 0; i < count; i++)
-	{
-		/* code */
-	}
-	
-	return (0);
+// void RPN::pushTmpToInputStack(void) {
+// 	while (!this->_inputStack.empty())
+// 	{
+// 		this->_inputStack.push(this->_inputStack.top());
+// 		this->_inputStack.pop();
+// 	}
+// }
+float RPN::operate(float nb1, float nb2, char op) {
+	if (op == '+')
+		return (nb2 + nb1);
+	else if (op == '-')
+		return (nb2 - nb1);
+	else if (op == '*')
+		return (nb2 * nb1);
+	else if (op == '/')
+		return (nb2 / nb1);
+	else
+		throw (RPN::BadOperator());
 }
 
+void	RPN::calculateStack(char op) {
+	if (this->_inputStack.size() == 2)
+	{
+		float	nb1 = this->_inputStack.top();
+		this->_inputStack.pop();
+		float	nb2 = this->_inputStack.top();
+		this->_inputStack.pop();
+		float result = operate(nb1, nb2, op);
+		this->_inputStack.push(result);
+	}
+}
+
+void RPN::addNumberToStack(string::iterator inputIt, unsigned int numberSize) {
+	string	numberString = string(inputIt, inputIt + numberSize);
+	float		number = atof(numberString.c_str());
+	if (number > 10)
+		throw(RPN::NumberTooBig(number));
+	this->_inputStack.push(number);
+	// cout << number << endl;
+}
+
+// Check the size of the current member and return it
+unsigned int	RPN::checkNumberSize(string::iterator inputIt) {
+	unsigned int numberSize = 0;
+	unsigned int dotCount = 0;
+	string inputCpy = &(*inputIt);
+	string ::iterator inputItCpy(inputCpy.begin());
+
+	if (*inputItCpy == '.')
+		throw(RPN::DotAtBeginningOfNumber());
+	for (; inputItCpy != inputCpy.end() && *inputItCpy != ' '; inputItCpy++)
+	{
+		if (*inputItCpy == '.')
+			dotCount++;
+		if (this->_NUMBERS.find_first_of(*inputItCpy) == std::string::npos && inputItCpy != inputCpy.end() && *inputItCpy != ' ')
+			throw(RPN::UnexpectedChar(*inputItCpy));
+		numberSize++;
+	}
+	if (dotCount > 1)
+		throw(RPN::TooManyDots());
+	if (*inputItCpy == '.')
+		throw(NumberEndsWithDot());
+	return (numberSize);
+}
+
+// Count the amout of numbers and operators in the input
+void RPN::countNumOPs(string input) {
+	string::iterator inputIt = input.begin();
+	int amountNb = 0;
+	int amountOp = 0;
+	unsigned int numberSize = 0;
+
+	int argPos = 0;
+	int lastArg = 0;
+	while (inputIt != input.end())
+	{
+		// Skip spaces
+		if (*inputIt == ' ')
+			inputIt++;
+		else if (this->_NUMBERS.find_first_of(*inputIt) != std::string::npos)
+		{
+			// Check order in respect of the last arg
+			if ( argPos > 1 && lastArg == 'n')
+				throw (RPN::ConsecutiveNumbers());
+			// Check Number length, add it to the stack
+			numberSize = checkNumberSize(inputIt);
+			addNumberToStack(inputIt, numberSize);
+			// Skip to the end of the number
+			std::advance(inputIt, numberSize);
+			amountNb++;
+			argPos++;
+			lastArg = 'n';
+		}
+		else if (this->_OPERATORS.find_first_of(*inputIt) != std::string::npos)
+		{
+			// Check order in respect of the last arg
+			if ((argPos == 0 || argPos == 1))
+				throw (RPN::OperatorsAtBeginning());
+			if ( argPos > 1 && lastArg == 'o')
+				throw (RPN::ConsecutiveOperators());
+			calculateStack(*inputIt);
+			amountOp++;
+			inputIt++;
+			argPos++;
+			lastArg = 'o';
+			// Check if nothing except a space is next to the operator
+			if (inputIt != input.end() && *inputIt != ' ')
+				throw(RPN::UnexpectedChar(*inputIt));
+			// Apply operator to the two numbers in the stack
+		}
+	}
+	if (amountNb < 2)
+		throw(RPN::NotEnoughNumbers());
+	if (amountNb > amountOp + 1)
+		throw(RPN::TooManyNumbers());
+	else if (amountNb < amountOp + 1)
+		throw(RPN::TooManyOperators());
+	
+}
 void RPN::parseInput(string input) {
 	if (input.empty())
 		throw(RPN::EmptyInput());
-	if (input.find_first_not_of(NUMOP" ") != std::string::npos)
+	if (input.find_first_not_of(this->_NUMOP + " ") != std::string::npos)
 		throw(RPN::IncorrectInput());
-	if (input.find_first_of(NUMBERS) == std::string::npos)
+	if (input.find_first_of(this->_NUMBERS) == std::string::npos)
 		throw(RPN::MissingNumbers());
-	if (input.find_first_of(OPERATORS) == std::string::npos)
+	if (input.find_first_of(this->_OPERATORS) == std::string::npos)
 		throw(RPN::MissingOperators());
 	
-	// Check if amount of number and operators correspond
-
-	// Check if their order is correct ex: 
-	// 1 1 + 2 - 3 - 1 is correct.
-	// 1 + 1 + 2 - 3 - 1 is NOT correct.
-
-	string::iterator inputIt = input.begin();
-
-	for (; inputIt != input.end(); inputIt++)
-	{
-		for (; *inputIt != ' '; inputIt++);
-		// Check if current pos is a nb or an op
-
-		// If its a number get it's size
-		checkNumberSize(inputIt);
-		// After checking the size get the number from inputIt to nbSize
-
-		//If it's an operator add it to the stack
-
-		// Push number or operator to the stack
-		// this->_inputStack.push(std::atof(*inputIt.c_str()));
-	}
-	cout << _inputStack.top() << endl;
-	
+	// Check if amount of number and operators correspond and if their order is correct while adding everything to the stack
+	countNumOPs(input);
+	cout << this->_inputStack.top() << endl;
 }
 
 RPN::~RPN() {
